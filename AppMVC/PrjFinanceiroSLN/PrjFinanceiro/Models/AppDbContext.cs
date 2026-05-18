069a5170103b9eb1368ad185e61bd9017060d35b
isabellysnt <isabelly.p.santos@aluno.senai.br>
2026-05-18 15:47:59 -0300

    certo

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
@@ -1,14 +1,19 @@
 ﻿
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace PrjFinanceiro.Models
 
 {
     public class AppDbContext : DbContext
     {
+       
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Agencia> Agencia { get; set; }
+        public DbSet<Agencia> Agencia{ get; set; }
+
+        public DbSet<Cargo> Cargo  { get; set; }
+       
 
     }
 }
