fbcfe78460b80c51bbbafffe3c441e5db8e16850
isabellysnt <isabelly.p.santos@aluno.senai.br>
2026-05-18 16:56:29 -0300

    h

--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/Models/AppDbContext.cs
@@ -1,14 +1,18 @@
 ﻿
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
 namespace PrjFinanceiro.Models
 
 {
+    
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Agencia> Agencia { get; set; }
+        public DbSet<cargo> Cargo { get; set; }
+        public DbSet<TipoConta> TipoConta { get; set; }
 
     }
 }
--- AppMVC/PrjFinanceiroSLN/PrjFinanceiro/obj/Debug/net8.0/PrjFinanceiro.AssemblyInfo.cs
+++ AppMVC/PrjFinanceiroSLN/PrjFinanceiro/obj/Debug/net8.0/PrjFinanceiro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrjFinanceiro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6730acbff2d53be11a39f193cdb6192245a6bcc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4db561e83eead9b1233ba76011505708d5b65973")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrjFinanceiro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrjFinanceiro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
